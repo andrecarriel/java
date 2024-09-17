2be2a1c7e720da65556b187c980b15b51d3804e8
Kaiser Design <160876744+andrecarriel@users.noreply.github.com>
2024-09-17 12:11:12 -0300

    Add files via upload

---
+++ carriel.js
@@ -0,0 +1,19 @@
+//Tipo number
+
+// Var era ultilizado para todas as variáveis
+const numero1 = 3
+// let é uma atualizaçao do var e é passivel de variação
+ const numero2 = 10
+ // const é uma maneira de declarar uma variável que irá permanecer constante
+const numero3 = 7
+
+const soma =numero1 + numero3
+const subtracao = numero02 - numero01
+const mutiplicacao = numero03 * numero02
+const divisao = numero02 / numero01
+
+console.log(soma)
+console.log(subtracao)
+console.log(mutiplicacao)
+console.log(divisao)
+
